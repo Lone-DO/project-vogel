113c0acb876a80387cd3ab6159df99d31416cd96
lone-DO <citizin@outlook.com>
2025-08-13 20:35:54 +0200

    Update `vogel` data for first 3 pages

--- src/assets/data/vogel.ts
+++ src/assets/data/vogel.ts
@@ -1,13 +1,47 @@
 import type { iVogel } from '@/models'
 
-export const vogel: iVogel[] = [
+export const vogels: iVogel[] = [
   {
     id: 1,
     name: 'Der GartenBaumläufer',
     size: '12-14 cm',
     weight: '11 g',
     sound: '',
-    data: [],
+    data: [
+      {
+        title: 'Aussehen und gesang',
+        description:
+          'Die Unterseite des Gartenbaum-läufers ist weißlich, die Oberseite dagegen braun-weiß gemustert -wie die Rinde der Bäume. So ist der kleine Waldvogel mit dem langen gebogenen Schnabel perfekt getarnt. Er singt eine hohe, kurze melodische Strophe: „Di-dideli-di-di."',
+      },
+      {
+        title: 'Lieblingsspeise',
+        description:
+          'Der Gartenbaumläufer sucht in den Ritzen der Rinde nach kleinen In-sekten. Darum sind ihm Bäume mit rauer Borke am liebsten.',
+        icon: 'flutterling',
+      },
+      {
+        title: 'Erstaunlich!',
+        description:
+          'Wie eine Maus läuft der Gartenbaum-läufer spiralförmig die Baumstämme hinauf. Oben angekommen, fliegt er direkt wieder nach unten - an den Stammfuß des nächsten Baums. Beim Klettern stützt er sich mit seinem langen Schwanz ab.',
+        icon: 'beetle',
+      },
+      {
+        title: '',
+        description: '',
+        image: 'nest',
+        image_description: 'Das Nest eines Gartenbaumlaufers',
+      },
+      {
+        title: 'Nest und nachwuchs',
+        description:
+          'Das Nest des Gartenbaumläufers ist hinter abstehenden Rindenstücken, in Spalten von Baumstämmen und in Höhlen oder Ritzen von Holzstapeln gut versteckt. Dort legt das Weib-chen meist fünf bis sechs Eier ab.',
+      },
+      {
+        title: 'Wenn es winter wird',
+        description:
+          'Der Gartenbaumläufer bleibt im Winter bei uns. Manchmal kommt er ans Futterhäuschen im Garten und lässt sich das Fettfutter schmecken. Eigentlich sind Baumläufer Einzel-gänger. Wird es jedoch sehr kalt, bilden sie Schlafgemeinschaften und kuscheln sich aneinander.',
+      },
+    ],
     position: 'bottom-right',
   },
   {
@@ -16,7 +50,41 @@ export const vogel: iVogel[] = [
     size: '23-26 cm',
     weight: '60-100 g',
     sound: '',
-    data: [],
+    data: [
+      {
+        title: 'AUSSEHEN UND GESANG',
+        description:
+          'Unser häufigster Specht ist leicht an seinem schwarz-weißen Gefieder, dem weißen Schulterfleck und den roten Federn am Unterschwanz zu er-kennen. Das Männchen hat zusätzlich einen roten Fleck am Nacken. Neben den kurzen „Kicks, kicks"-Rufen macht der Buntspecht vor allem im Frühling auf sich aufmerksam, wenn er mit seinem Schnabel gegen Baum-stämme trommelt.',
+      },
+      {
+        title: 'LIEBLINGSSPEISE',
+        description:
+          'Neben Insekten und Larven, die der Vogel mit seinem Schnabel aus mor-schem Holz holt, frisst er auch Nüsse und Samen aus Zapfen.',
+      },
+      {
+        title: 'ERSTAUNLICH!',
+        description:
+          'Spechte haben oft eine eigene Schmiede, eine Spechtschmiede. Das sind Rindenspalten oder Astgabeln. Dort klemmen die Tiere Nüsse oder Zapfen ein, um sie dann mit dem Schnabel aufzuhacken.',
+        icon: 'tree',
+      },
+      {
+        title: 'NEST UND NACHWUCHS',
+        description:
+          'Der Buntspecht hämmert eine Höhle in einen morschen oder sowieso wei-chen Baumstamm. Das Weibchen legt vier bis sieben weiße Eier hinein.',
+        icon: 'catepillar',
+      },
+      {
+        title: 'WENN ES WINTER WIRD',
+        description:
+          'Buntspechte bleiben im Winter bei uns und kommen auf der Suche nach Nahrung auch in unsere Gärten. Weil sie Fettfutter und Nüsse mögen, hängen sie sich manchmal sogar an Meisenknödel.',
+      },
+      {
+        title: '',
+        description: '',
+        image: '',
+        image_description: 'Ein junger Buntspecht schaut aus seiner Baumhöhle.',
+      },
+    ],
     position: 'left-bottom',
   },
   {
@@ -25,7 +93,33 @@ export const vogel: iVogel[] = [
     size: '15-19 cm',
     weight: '26 g',
     sound: '',
-    data: [],
+    data: [
+      {
+        title: 'AUSSEHEN UND GESANG',
+        description:
+          'An seiner hervorstehenden Brust, dem dicken Hals und dem kleinen Kopf erkennst du den Gimpel sofort. Beim Männchen sind Brust- und Bauchgefieder hellrot, Kopfplatte, Gesicht und der kräftige Schnabel schwarz, der Rücken ist grau und im Flug leuchtet der weiße Bürzel auf. Beim Weibchen ist alles bräunlicher, blasser und unscheinbarer gefärbt. Der Gimpel singt eine leise, unregelmäßige Folge von klaren Pfeiftönen.',
+      },
+      {
+        title: 'LIEBLINGSSPEISE',
+        description:
+          'Mit seinem dicken Körnerfresser-schnabel frisst der Gimpel am liebsten Samen, Knospen und Beeren.',
+      },
+      {
+        title: 'ERSTAUNLICH!',
+        description:
+          'Sein Gesang ist dem Gimpel nicht angeboren, er lernt ihn als kleines Küken von seinen Eltern. Darum wurden viele Gimpel früher von Hand aufgezogen, weil die Menschen ihnen eine bestimmte Melodie beibringen wollten. So lebten die Waldvögel häufig eingesperrt im Käfig.',
+      },
+      {
+        title: 'NEST UND NACHWUCHS',
+        description:
+          'Der Gimpel baut sein Nest in Nadel-gehölzen, manchmal auch in dichten Gebüschen. Meist liegt es in ein bis drei Metern Höhe. Vier bis sechs Eier legt das Weibchen hinein.',
+      },
+      {
+        title: 'WENN ES WINTER WIRD',
+        description:
+          'Der Gimpel ist ein Standvogel und bleibt im Winter hier. Auf der Suche nach Futter streift er ein bisschen umher und kommt manchmal auch zum Futterhäuschen in den Garten.',
+      },
+    ],
     position: 'left-top',
   },
 ]
