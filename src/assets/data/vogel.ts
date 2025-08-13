ec7cf1d5effd70548a707477aa75027f691b2d79
lone-do <citizin@outlook.com>
2025-08-13 03:01:03 +0200

    Data - init vogel data and typings

---
+++ src/assets/data/vogel.ts
@@ -0,0 +1,31 @@
+import type { iVogel } from '@/models'
+
+export const vogel: iVogel[] = [
+  {
+    id: 1,
+    name: 'Der GartenBaumläufer',
+    size: '12-14 cm',
+    weight: '11 g',
+    sound: '',
+    data: [],
+    position: 'bottom-right',
+  },
+  {
+    id: 2,
+    name: 'Der Buntspecht',
+    size: '23-26 cm',
+    weight: '60-100 g',
+    sound: '',
+    data: [],
+    position: 'left-bottom',
+  },
+  {
+    id: 3,
+    name: 'Der Gimpel',
+    size: '15-19 cm',
+    weight: '26 g',
+    sound: '',
+    data: [],
+    position: 'left-top',
+  },
+]
---
+++ src/models/index.d.ts
@@ -0,0 +1,17 @@
+export interface iVogelArticle {
+  icon?: string
+  title: string
+  description: string
+  image?: string
+  image_description?: string
+}
+
+export interface iVogel {
+  id: number
+  name: string
+  position: string
+  sound: string
+  size: string
+  weight: string
+  data: iVogelArticle[]
+}
