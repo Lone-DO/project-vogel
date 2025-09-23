657770a8f12deadf332ec6f6eea58a6479dbac65
lone-DO <citizin@outlook.com>
2025-09-24 01:08:59 +0200

    PlayerStore - init

---
+++ src/components/VogelPlayer.vue
@@ -0,0 +1,21 @@
+<script lang='ts' setup>
+import { usePlayerStore } from '@/stores/player';
+import { onMounted, useTemplateRef } from 'vue';
+
+const playerStore = usePlayerStore()
+const element = useTemplateRef('player')
+onMounted(() => {
+  playerStore.element = element.value
+})
+</script>
+
+<template>
+  <div id='vogel-player'>
+    <img src="@/assets/images/speaker.webp" alt="speaker image" />
+    <audio ref='player' :src="playerStore.currentSound || ''" :muted='!playerStore.enabled' />
+  </div>
+</template>
+
+<style lang='scss'>
+#vogel-player {}
+</style>
