334f3549280cef58d0477c947b7db1131c2f284e
lone-DO <citizin@outlook.com>
2025-09-23 02:24:00 +0200

    Slider Component - Init

---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,69 @@
+<script setup lang='ts'>
+
+const model = defineModel<boolean>()
+const $emit = defineEmits(['update:value'])
+function update(event: unknown) {
+  $emit('update:value', event)
+}
+</script>
+
+<template>
+  <label class='toggle-switch' :data-checked='model || null'>
+    <span>ON</span>
+    <div class='toggle-switch__controller'>
+      <input name='volume-toggle' v-model='model' type="checkbox" @input='update' />
+      <i class='toggle-switch__icon'></i>
+    </div>
+    <span>OFF</span>
+  </label>
+</template>
+
+<style lang='scss' scoped>
+.toggle-switch {
+  min-width: 1rem;
+  max-width: 1rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  cursor: pointer;
+
+  input {
+    top: 2px;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+
+  &__controller {
+    flex: 1;
+    display: flex;
+    position: relative;
+    justify-content: center;
+
+
+    width: 100%;
+    padding: 2px;
+    min-height: 2rem;
+    border-radius: 1rem;
+    background-color: $green;
+    @include BoxShadow(inset 0px 0px 4px 0px rgba($color: black, $alpha: 0.75));
+  }
+
+  &__icon {
+    width: 12px;
+    height: 12px;
+    border-radius: 100%;
+    @include BoxShadow(inset 0px 0px 4px 0px rgba($color: black, $alpha: 0.75));
+    transition: all 100ms ease-in-out
+  }
+
+  &[data-checked] &__icon {
+    transform: translateY(0%);
+  }
+
+  &:not([data-checked]) &__icon {
+    transform: translateY(100%);
+  }
+}
+</style>
---
+++ src/components/VogelController.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ToggleSwitch from './ToggleSwitch.vue';
+
+const active = ref(true)
+</script>
+
+<template>
+  <div id='vogel-controller'>
+    <div id='vogel-controller__description'>
+      <p>
+        <span>CAUTION:</span>
+        <span>BATTERIES TO BE INSTALLED</span>
+        <span>BY ADULTS ONLY</span>
+        <span>AG13/LR44 DC 1.5VX3=4.5V</span>
+      </p>
+
+    </div>
+    <ToggleSwitch v-model='active' />
+  </div>
+</template>
+
+<style lang='scss' scoped>
+#vogel-controller {
+  @include Flex($gap: 0, $justify: space-between);
+
+  @include Desktop {
+    width: 100%;
+    font-size: 0.5rem;
+  }
+
+  &__description {
+    display: none;
+    @include BoxShadow(inset 0px 0px 2px 1px rgba($color: black, $alpha: 0.5));
+
+    @include Desktop {
+      padding: 4px;
+      display: block;
+      border-radius: 4px;
+
+      p {
+        display: flex;
+        text-align: center;
+        align-items: center;
+        flex-direction: column;
+      }
+    }
+  }
+}
+</style>
